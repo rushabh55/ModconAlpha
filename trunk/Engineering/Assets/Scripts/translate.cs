d5545c3a96d53c32f4a615690e22e1dba02510da
Rushabh Gosar <rushabh.techie@gmail.com>
2014-08-02 17:49:36 -0400

    Patching my changes

--- trunk/Engineering/Assets/Scripts/translate.cs
+++ trunk/Engineering/Assets/Scripts/translate.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class translate : MonoBehaviour {
 
+    public SpringJoint _joint = null;
+    public Transform _target = null;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,12 @@ public class translate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(new Vector3(1, 0, 0));
+        var t = this.transform.rotation;
+        this.transform.rotation.Set(0, 0, 0, 0);
+        
+        _joint.connectedAnchor = Vector3.MoveTowards(_joint.connectedAnchor, _target.position, Time.deltaTime * 1);
+
+        this.transform.rotation = t;
+    //    this.transform.Translate(new Vector3(1, 0, 0));
 	}
 }
