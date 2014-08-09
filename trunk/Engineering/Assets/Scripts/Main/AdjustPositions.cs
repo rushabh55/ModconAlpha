5bbc5e41c5e6140e79ad6f9fcbf4902bcacc27d2
Rushabh Gosar <rushabh.techie@gmail.com>
2014-08-08 22:46:36 -0400

    Reverting my scripts.

--- trunk/Engineering/Assets/Scripts/Main/AdjustPositions.cs
+++ trunk/Engineering/Assets/Scripts/Main/AdjustPositions.cs
@@ -10,7 +10,6 @@ public class AdjustPositions : MonoBehaviour
 	void Start () 
     {
         Reposition();
-
 	}
 
     void Awake()
@@ -22,12 +21,12 @@ public class AdjustPositions : MonoBehaviour
     {
         _start.transform.position = pole1.transform.position;
         var pos = _start.transform.position;
-        pos.y += (float)pole1.renderer.bounds.size.y / 2 - ((int)((BoxCollider)_player.collider).size.y << 1);
+        pos.z += (float)pole1.renderer.bounds.size.y / 2 * (float)pole1.transform.localScale.y - ((int)((BoxCollider)_player.collider).size.y << 1);
         _start.transform.position = pos;
 
         _target.transform.position = pole2.transform.position;
         pos = _target.transform.position;
-        pos.y += (float)pole2.renderer.bounds.size.y / 2 - ((int)((BoxCollider)_player.collider).size.y << 1);
+        pos.z += (float)pole2.renderer.bounds.size.y / 2 * (float)pole2.transform.localScale.y - ((int)((BoxCollider)_player.collider).size.y << 1);
         _target.transform.position = pos;
     }
 	// Update is called once per frame
--- trunk/Engineering/Assets/Scripts/Main/WorkerController.cs
+++ trunk/Engineering/Assets/Scripts/Main/WorkerController.cs
@@ -1,45 +1,45 @@
-﻿using UnityEngine;
-using System.Collections;
-using Assets.Scripts.Base;
-using Assets.Scripts.Serializable;
+﻿//using UnityEngine;
+//using System.Collections;
+//using Assets.Scripts.Base;
+//using Assets.Scripts.Serializable;
 
-public class WorkerController : MonoBehaviour , IExplode
-{
-    public ParticleSystem _partSystem = null;
-    public GameObject _player = null;
-    private TransformCollection _transform = new TransformCollection();
-	// Use this for initialization
-	void Start () {
-        _transform.position = _player.transform.position;
-        _transform.rotation = _player.transform.rotation;
-        _transform.scale = _player.transform.localScale;
-	}
+//public class WorkerController : MonoBehaviour , IExplode
+//{
+//    public ParticleSystem _partSystem = null;
+//    public GameObject _player = null;
+//    private TransformCollection _transform = new TransformCollection();
+//    // Use this for initialization
+//    void Start () {
+//        _transform.position = _player.transform.position;
+//        _transform.rotation = _player.transform.rotation;
+//        _transform.scale = _player.transform.localScale;
+//    }
 	
-	// Update is called once per frame
-	void Update () {
-        if ( Input.touchCount > 0)
-        {
-            var delta = Input.acceleration;
-            //_player.transform.Translate(-delta.y, 0, delta.x);
-          //  DebugPanel.AddText(delta.ToString() + _player.transform.position.ToString());
-        }
-        if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 1)
-        {
+//    // Update is called once per frame
+//    void Update () {
+//        if ( Input.touchCount > 0)
+//        {
+//            var delta = Input.acceleration;
+//            //_player.transform.Translate(-delta.y, 0, delta.x);
+//          //  DebugPanel.AddText(delta.ToString() + _player.transform.position.ToString());
+//        }
+//        if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 1)
+//        {
             
-        }
-	}
+//        }
+//    }
 
-    public void Explode()
-    {
-        _partSystem.Emit(2500);
-        _player.renderer.active = false;
-    }
+//    public void Explode()
+//    {
+//        _partSystem.Emit(2500);
+//        _player.renderer.active = false;
+//    }
 
-    public void Respawn()
-    {
-        _player.renderer.active = true;
-        _player.transform.position = _transform.position;
-        _player.transform.rotation = _transform.rotation;
-        _player.transform.localScale = _transform.scale;
-    }
-}
+//    public void Respawn()
+//    {
+//        _player.renderer.active = true;
+//        _player.transform.position = _transform.position;
+//        _player.transform.rotation = _transform.rotation;
+//        _player.transform.localScale = _transform.scale;
+//    }
+//}
